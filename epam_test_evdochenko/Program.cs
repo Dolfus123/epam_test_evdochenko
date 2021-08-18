85c75f3529544f5c0d591ca4ac770a1d3e08fafa
rovastall@gmail.com <rovastall@gmail.com>
2021-08-18 18:51:45 +0300

    Добавьте файлы проекта.

---
+++ epam_test_evdochenko/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_test_evdochenko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.SetWindowSize(170, 50);
+
+                List<Item> list = new List<Item>()
+            {
+                new Product(2, 21, 800 ),
+                new Product(4, 10, 750, new PercentDiscount(5)),
+                new Product(2, 2, 1000, new DeliveryDiscount(50)),
+                new Product(10, 12, 1500, new BonusDiscount(100)),
+                new Product(5, 18, 500),
+                new Product(1, 10, 950, new PercentDiscount(15)),
+                new Product(4, 25, 700, new PercentDiscount(25)),
+                new Product(1, 4, 1100, new DeliveryDiscount(50)),
+                new Product(15, 15, 600, new BonusDiscount(100)),
+                new Product(10, 13, 400, new BonusDiscount(100)),
+             };
+
+                int k = 0;
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine("{0}", item);
+                    if (item.DayOfMonth == 10)
+                        k++;
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("There was {0} purchase(s) at 10th of this month", k);
+
+                list.Sort();
+                Console.WriteLine();
+                Console.WriteLine("Sorting by date:");
+                Console.WriteLine();
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Sorting by discount:");
+                Console.WriteLine();
+
+                var groupBy = list.GroupBy(x => x.Discount != null ? x.Discount.GetType().Name : "No discount").ToList();
+
+                foreach (var v in groupBy)
+                {
+                    Console.WriteLine("{0} :", v.Key);
+
+                    foreach (var item in v)
+                    {
+                        Console.WriteLine(item);
+                    }
+                    Console.WriteLine();
+                }
+
+
+                Console.ReadKey();
+            }
+        }
+    }
+ }
+
---
+++ epam_test_evdochenko/item.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace epam_test_evdochenko
+{
+    abstract class Item : IComparable<Item>
+    {
+        public string Name { get; private set; }
+        public byte Count { get; private set; }
+        public byte DayOfMonth { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal TotalPrice
+        {
+            get
+            {
+                if (Discount == null)
+                    return Price * Count;
+                else
+                    return Discount.SetPrice(Price, Count);
+            }
+            private set { _ = Price * Count; }
+        }
+
+        public IDiscount Discount;
+
+        protected Item(string name, byte count, byte date, decimal price)
+        {
+            Name = name;
+            Count = count;
+            DayOfMonth = date;
+            Price = price;
+        }
+
+        protected Item(string name, byte count, byte date, decimal price, IDiscount discount)
+        {
+            Name = name;
+            Count = count;
+            DayOfMonth = date;
+            Price = price;
+            Discount = discount;
+        }
+
+        public void SetDiscount(IDiscount discount)
+        {
+            Discount = discount;
+        }
+
+        public int CompareTo(Item other)
+        {
+            return DayOfMonth.CompareTo(other.DayOfMonth);
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}|\t Count: {1}|\t Price: {2} BYR|\t Date of the month: {3}|\t Total Price: {4} ",
+                Name, Count, Price, DayOfMonth, TotalPrice) + (Discount == null ? "" : Discount.ToString());
+        }
+    }
+
+    class Product : Item
+    {
+        public Product(byte count, byte date, decimal price, IDiscount discount) : base("Product_1", count, date, price, discount)
+        {
+        }
+
+        public Product(byte count, byte date, decimal price) : base("Product_1", count, date, price)
+        {
+        }
+    }
+
+    internal interface IDiscount
+    {
+        decimal SetPrice(decimal price, byte count);
+    }
+
+    class PercentDiscount : IDiscount
+    {
+        private readonly int Percent;
+
+        public PercentDiscount(int percent)
+        {
+            Percent = percent;
+        }
+
+        public decimal SetPrice(decimal price, byte count)
+        {
+            return price * count * (1 - Percent / 100);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("The discount is: {0}%", Percent);
+        }
+    }
+
+    class DeliveryDiscount : IDiscount
+    {
+        private readonly int DeliveryCost;
+
+        public DeliveryDiscount(int deliverycost)
+        {
+            DeliveryCost = deliverycost;
+        }
+
+        public decimal SetPrice(decimal price, byte count)
+        {
+
+            return (price * count) - DeliveryCost;
+        }
+
+        public override string ToString()
+        {
+
+            return string.Format("The discount for shipping is: {0} BYR ", DeliveryCost);
+        }
+    }
+
+    class BonusDiscount : IDiscount
+    {
+
+        private readonly int Bonus;
+
+        public BonusDiscount(int bonus)
+        {
+            Bonus = bonus;
+        }
+
+        public decimal SetPrice(decimal price, byte count)
+        {
+
+            return (price * count) - Bonus;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("The discount is equal to the value of the bonus item: {0} BYR", Bonus);
+        }
+    }
+}
